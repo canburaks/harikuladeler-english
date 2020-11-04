5453cf39f8fef60a56acbd4942d7d7910c4a4776
Can Burak S <info@livicomturkiye.com>
2020-11-04 07:34:55 +0300

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { useEffect } from 'react'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -6,12 +7,20 @@ class MyDocument extends Document {
     return { ...initialProps }
   }
 
+    useEffect(() => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+      gtag('config', 'G-LYECJRL21W');
+    },[])
+
   render() {
     return (
       <Html lang="en-">
         <Head >
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-LYECJRL21W"></script>
+
           {/* 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YWQNTGMG9S"></script>
         <script data-ad-client="ca-pub-9259748524746137" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5f8f8affd13d373469d736e5" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossOrigin="anonymous"></script>
           */}
